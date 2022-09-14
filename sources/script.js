16061bba93173b8b5501613e990e1868cd57576d
Mariia Artamonova <mariia.artamonova@ubisoft.com>
2022-09-15 00:16:48 +0300

    updated script.js

--- sources/script.js
+++ sources/script.js
@@ -1,56 +1,47 @@
 const content = [
   {
-    id: "0",
     img: "./images/image1.1.jpg",
     imgHover: "./images/image1.2.jpg",
     text: `відчувати себе героєм фільму "Кладовище домашніх тварин" (ps а за вікном, на хвилиночку, другий поверх)`,
   },
   {
-    id: "1",
     img: "./images/image2.1.jpg",
     imgHover: "./images/image2.2.jpg",
     text: "отримувати вельми специфічний сніданок, зате впольований лапками і з любов'ю",
   },
   {
-    id: "2",
     img: "./images/image3.1.jpg",
     imgHover: "./images/image3.2.jpg",
     text: "вдавати, що кіт на даху твого дому не твій кіт, і що він зовсім не кричить на всю вулицю",
   },
   {
-    id: "3",
     img: "./images/image4.1.jpg",
     imgHover: "./images/image4.2.jpg",
     text: "прокидаєшся, а тут...",
   },
   {
-    id: "4",
     img: "./images/image5.1.jpg",
     imgHover: "./images/image5.2.jpg",
     text: "забути, що таке приватна власність",
   },
   {
-    id: "5",
     img: "./images/image6.1.jpg",
     imgHover: "./images/image6.2.jpg",
     text: "ніколи не забувати, що миле котятко - лютий хижак",
   },
   {
-    id: "6",
     img: "./images/image7.1.jpg",
     imgHover: "./images/image7.2.jpg",
     text: "рятувати живу мишку по даху",
   },
   {
-    id: "7",
     img: "./images/image8.1.jpg",
     imgHover: "./images/image8.2.jpg",
     text: "жизнь-боль-тлєн",
   },
   {
-    id: "8",
-    img: '"./images/image9.1.jpg"',
-    imgHover: '"./images/image9.2.jpg"',
+    img: "./images/image9.1.jpg",
+    imgHover: "./images/image9.2.jpg",
     text: "дуже багато милоти... і крінжі",
   }
 ];
